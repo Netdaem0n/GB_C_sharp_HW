8c68c8ddf44786d184bfe7610cd570fffd49b581
Netdaem0n <bakalets@gmail.com>
2022-07-06 18:47:35 +1100

    -

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,34 @@ Array.Reverse(b);
 c = new string(b);
 if (c == a)
 {
-   Console.WriteLine("Yes"); 
+    Console.WriteLine("Yes"); 
 }
 else {Console.WriteLine("No");}
 
+Console.WriteLine("Задача 21");
+//Напишите программу, которая принимает на вход координаты двух точек
+//и находит расстояние между ними в 3D пространстве.
+Console.WriteLine("Введите координаты двух точек в формате, каждое с новой строки");
+double[] abc = new double[3];
+double[] def = new double[3];
+abc[0] = Convert.ToDouble(Console.ReadLine());
+abc[1] = Convert.ToDouble(Console.ReadLine());
+abc[2] = Convert.ToDouble(Console.ReadLine());
+def[0] = Convert.ToDouble(Console.ReadLine());
+def[1] = Convert.ToDouble(Console.ReadLine());
+def[2] = Convert.ToDouble(Console.ReadLine());
+double distanse;
+distanse = Math.Sqrt(
+    Math.Pow(def[0] - abc[0], 2) +
+    Math.Pow(def[1] - abc[1], 2) +
+    Math.Pow(def[2] - abc[2], 2)
+);
+Console.WriteLine(distanse);
+
+Console.WriteLine("Задача 23");
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    Console.Write(Math.Pow(i, N) + " ");
+}
