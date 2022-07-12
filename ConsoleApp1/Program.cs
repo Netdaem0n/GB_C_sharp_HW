ffbf5fd3abbaa73242d5fcc65c8bbaf5fc81c07c
Netdaem0n <bakalets@gmail.com>
2022-07-12 20:09:51 +1100

    -

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,66 +1,26 @@
-﻿Console.WriteLine("Задача 10");
-int temp;
-h10:
-Console.Write("Введите трехзначное число ");
-string a = Console.ReadLine();
+﻿using System.Data;
+
+Console.WriteLine("Задача 41");
+//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
 
 
-if (a.Length == 3 & int.TryParse(a, out temp))
+
+Console.Write("Введите числа через разделитель запрятую (пример: 3, 4, -7)");
+
+string user_text = Console.ReadLine();
+string[] numbers = user_text.Split(',');
+
+void wtf(string[] numbers)
 {
-    Console.Write($"Вы ввели {a}, Вторая цифра ");
-    Console.Write(a[1] + "\n");
-    Console.WriteLine("------------------");
-}
-else
-{
-    Console.WriteLine($"Введите трехзначное число, Вы ввели {a}");
-    goto h10;}
-
-
-Console.WriteLine("Задача 13");
-h13:
-Console.Write("Введите число ");
-a = Console.ReadLine();
-if (int.TryParse(a, out temp))
-{
-    if (a.Length >= 3)
+    int temp_ok = 0;
+    for (int i = 0; i < numbers.Length; i++)
     {
-        Console.Write($"Вы ввели {a}, третья цифра ");
-        Console.Write(a[2] + "\n");
-        Console.WriteLine("------------------");
+        int temp = Convert.ToInt32(numbers[i]);
+        if (temp > 0) temp_ok++;
     }
-    else
-    {
-        Console.WriteLine($"Вы ввели {a}, третьей цифры нет");
-    }
+    Console.WriteLine("Чисел больше 0 : " + temp_ok);
 }
 
-else
-{
-    Console.WriteLine($"Введите число, Вы ввели {a}");
-    goto h13;}
-
-
-Console.WriteLine("Задача 15");
-h15:
-Console.Write("Введите день недели (число) ");
-a = Console.ReadLine();
-if (int.TryParse(a, out temp))
-{
-    temp = Convert.ToInt32(a);
-    if (temp == 6 | temp == 7)
-    {
-        Console.WriteLine($"Ура, Выходной!!!!");
-        Console.WriteLine("------------------");
-    }
-    else
-    {
-        Console.WriteLine($"Работаем, к сожалению {a} день недели не выходной");
-    }
-}
-
-else
-{
-    Console.WriteLine($"Введите число, Вы ввели {a}");
-    goto h15;}
-
+wtf(numbers);
